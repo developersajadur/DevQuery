4638fcb49a3d26125d375eb77ee68003104be882
Ashraful Islam <155339396+theashrafislam@users.noreply.github.com>
2024-09-19 01:39:20 +0600

    Update GetQuestions.jsx

--- src/Components/Questions/GetQuestions.jsx
+++ src/Components/Questions/GetQuestions.jsx
@@ -1,10 +1,10 @@
 import axios from "axios";
-const url = process.env.WEB_URL;
+// const url = process.env.WEB_URL;
 
 // This function will fetch the questions using axios
 export const getQuestions = async () => {
   try {
-    const response = await axios.get(`${url}/questions/api/get`);
+    const response = await axios.get("/questions/api/get");
     const questions = response?.data?.questions;
     return questions;
   } catch (error) {
