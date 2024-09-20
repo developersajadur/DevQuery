7c96794433af317dd0008bac8c4b3a7d4fab87ee
itzmesojib <itzmesojib@gmail.com>
2024-09-20 21:08:26 +0600

    added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,3 @@
-import localFont from "next/font/local";
 import "./globals.css";
 import ReactQueryProvider from "@/lib/Providers/ReactQueryProvider";
 import Navbar from "@/Components/Shared/Navbar";
@@ -7,17 +6,7 @@ import Footer from "@/Components/Shared/Footer";
 import AuthProvider from "@/lib/Providers/AuthProvider";
 import { Toaster } from "react-hot-toast";
 
-// Load fonts
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+
 
 // Metadata for SEO (server-side)
 export const metadata = {
