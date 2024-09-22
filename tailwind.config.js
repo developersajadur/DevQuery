8502b89198a1fe5a0dddee5c1fc96a6d916de843
itzmesojib <itzmesojib@gmail.com>
2024-09-22 16:22:57 +0600

    added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+import flowbite from "flowbite-react/tailwind";
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/flowbite-react/**/*.js",
+    flowbite.content(),
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('flowbite/plugin'),
+    flowbite.plugin(),
   ],
 };
