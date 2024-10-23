d7e72c35181e48ad02be87cd937fac9ba74e6cb7
itzmesojib <itzmesojib@gmail.com>
2024-10-23 15:47:26 +0600

    fix some isuue for live link

--- src/lib/uploadthing.js
+++
@@ -1,8 +0,0 @@
-import { UploadClient } from 'uploadthing';
-
-// Create the UploadClient instance
-const uploadClient = new UploadClient({
-  apiKey: process.env.UPLOADTHING_TOKEN, 
-});
-
-export default uploadClient; 
